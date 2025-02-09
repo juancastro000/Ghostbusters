6806f9086127e32230a867a74055884642b2a222
juancastro000 <cdyutub@gmail.com>
2025-02-09 17:01:03 +0100

    main

--- src/main/java/com/ghostdevs/ghostbusters/App.java
+++ src/main/java/com/ghostdevs/ghostbusters/App.java
@@ -6,11 +6,11 @@ import com.ghostdevs.ghostbusters.view.MenuPrincipal;
 
 public class App {
     public static void main(String[] args) {
-        // Inicializamos el contenedor de fantasmas y el controlador
+
         ContenedorEctoplasmico contenedor = new ContenedorEctoplasmico();
         FantasmaController controller = new FantasmaController(contenedor);
 
-        // Iniciamos el menú principal
+
         MenuPrincipal menu = new MenuPrincipal(controller);
         menu.mostrarMenu();
   
